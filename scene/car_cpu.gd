dd4d384f8580ad1a5b5961a643bd48a056729d55
Matt Pierre <mattpierre@Matts-MacBook-Pro.local>
2024-02-24 15:16:42 -0400

    update

---
+++ scene/car_cpu.gd
@@ -0,0 +1,11 @@
+extends CharacterBody2D
+#
+#@onready var pathFollow : PathFollow2D = $".."
+#
+#@onready var speed = 1
+#
+#func _ready():
+	#pathFollow.rotates = true
+#
+#func _process(delta: float) -> void:
+	#pathFollow.progress += speed
